c707e02e8f24bd6bc1442a5a5c52e7af797bc601
Perdanaeka <perdanaeka99@gmail.com>
2020-12-20 20:12:51 +0700

    penambahan algoritma jaccard similiarity

--- koneksi.php
+++ koneksi.php
@@ -3,9 +3,9 @@
 
 $host  = "localhost";
 $username  = "root";
-$password  = "root";
+$password  = "";
 $database  = "stki";
-$koneksi  = mysqli_connect($host,  $username,  '', $database);
+$koneksi  = mysqli_connect($host,  $username,  $password, $database);
 $pilihdatabase  = mysqli_select_db($koneksi, "stki");
 if  ($pilihdatabase) echo "<h3>Koneksi Berhasil </h3><hr><br/>";
 else echo "Koneksi Gagal <br/>";
